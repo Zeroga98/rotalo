22d5cc0526fde72ffa1df7ec5c986cab5263f01e
Sebastian Mora Gallego <sebastian@MacBook-Pro-de-administrador.local>
2018-02-20 15:41:24 -0500

    inicio subir oferta

--- src/commons/interceptors/token.interceptor.ts
+++ src/commons/interceptors/token.interceptor.ts
@@ -17,6 +17,6 @@ export class TokenInterceptor implements HttpInterceptor {
     }
 
     private getToken():string{
-        return 'TsLCvqaVTeyVejQxxYvu';
+        return 'PyxRby2giXFjzjPcGoeS';
     }
 }
