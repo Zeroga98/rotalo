b26be89c3d7389b216fa6aeb61cc79bde403c630
Mario Fernandez <jorge.fernandez@pragma.com.co>
2018-02-23 08:51:16 -0500

    se crea metodo para obtener usuario en user service

--- src/pages/profile/hobbies/hobbies.page.ts
+++ src/pages/profile/hobbies/hobbies.page.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { UserService } from "../../../services/user.service";
 
 @Component({
-  selector: 'app-hobbies',
-  templateUrl: 'hobbies.page.html',
-  styleUrls: ['hobbies.page.scss']
+  selector: "app-hobbies",
+  templateUrl: "hobbies.page.html",
+  styleUrls: ["hobbies.page.scss"]
 })
 export class HobbiesPage implements OnInit {
-
-  constructor() { }
+  public hobbiesForm: FormGroup;
+  public errorChange: String;
+  public messageChange: String;
+  public userHobbie: any = "";
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
+    this.hobbiesForm = new FormGroup({
+      hobbie: new FormControl()
+    });
   }
 
+  async loadHobbie() {
+    this.userHobbie = await this.userService.getInfoUser();
+  }
+
+  onSubmit() {
+
+  }
 }
