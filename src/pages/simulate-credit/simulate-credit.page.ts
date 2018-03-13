a79ec5d7d0288aed35a4a457b3c6598507605b24
Mario Fernandez <jorge.fernandez@pragma.com.co>
2018-03-13 12:02:48 -0500

    se cambia patron para cedula extranjeria

--- src/pages/simulate-credit/simulate-credit.page.ts
+++ src/pages/simulate-credit/simulate-credit.page.ts
@@ -11,6 +11,7 @@ import { ProductInterface } from "../../commons/interfaces/product.interface";
 import { ProductsService } from "../../services/products.service";
 import { SettingsService } from "../../services/settings.service";
 import { SimulateCreditService } from "../../services/simulate-credit.service";
+import { ROUTES } from "../../router/routes";
 
 /**Validator**/
 function princeVehicleValidatorMax(
@@ -157,6 +158,8 @@ export class SimulateCreditPage implements OnInit {
 
     this.simulateCreditService.simulateCredit(infoVehicle).then(response => {
       const result = confirm('¡Relájate! Lasolicitud ya fue enviada y pronto un integrante de nuestro equipo se pondrá en contacto para contarte el resultado.\n Si tienes alguna duda, contáctanos a través de nuestro correo\n servicioalcliente@rotalo.com.');
+      if (!result) { return; }
+      this.router.navigate([`/${ROUTES.PRODUCTS.LINK}/${ROUTES.PRODUCTS.FEED}`]);
     })
     .catch(httpErrorResponse => {});
   }
