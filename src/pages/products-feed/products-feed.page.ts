9ab345211f3ea2a6fe30dec4a77a3e1258d4e042
sebastian.mora <sebastian.mora@pragma.com.co>
2018-03-27 15:24:34 -0500

    fixes

--- src/pages/products-feed/products-feed.page.ts
+++ src/pages/products-feed/products-feed.page.ts
@@ -186,7 +186,7 @@ export class ProductsFeedPage implements OnInit, OnDestroy {
   private updateProducts(newProducts: Array<ProductInterface>){
     this.waitNewPage ? this.addNewPage(newProducts) : this.products = [].concat(newProducts);
     this.waitNewPage = false;
-    //this.updateMasonry();
+    this.updateMasonry();
   }
 
   addNewPage(newProducts){
@@ -219,7 +219,7 @@ export class ProductsFeedPage implements OnInit, OnDestroy {
   private updateMasonry(){
     if(this.masonryRef.layout) setTimeout(() => {
       this.masonryRef.layout();
-    },0.5);
+    }, 1);
   }
 
   private validateStateScrollInfinite(products:ProductInterface){
