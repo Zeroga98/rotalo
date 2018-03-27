0e81eabb97f3a47fa969e1e9fc7d65f844710111
Mario Fernandez <jorge.fernandez@pragma.com.co>
2018-03-27 16:04:01 -0500

    se comenta libreria para ajuste de imagenes en el Feed

--- src/services/configuration.service.ts
+++ src/services/configuration.service.ts
@@ -9,6 +9,13 @@ export class ConfigurationService {
     // server: 'api.rotalo.dev.localhost:3000',
     'inactivityLimit': 86400 // seconds
   };
+  /*
+   api = {
+      'protocol': "https",
+      'server': "api.rotalo.co",
+       'inactivityLimit': 86400 // seconds
+      };
+  */
   jsonApiHeaders = {
     'Accept': "application/vnd.api+json",
     "Content-Type": "application/vnd.api+json"
