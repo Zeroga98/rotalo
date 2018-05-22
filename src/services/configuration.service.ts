5c9892f18511c4b8475c10ea34c24137d731e0bf
Mario Fernandez <jorge.fernandez@pragma.com.co>
2018-05-22 15:21:37 -0500

    se hace merge con ultima version rotalo consumiendo servicios en java

--- src/services/configuration.service.ts
+++ src/services/configuration.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class ConfigurationService {
-  api = {
+  /*api = {
     'protocol': 'https',
     'server': 'apistg.rotalo.co:1443/gateway/v1',
      'inactivityLimit': 86400 // seconds
@@ -12,7 +12,7 @@ export class ConfigurationService {
     'protocol': 'https',
     'server': 'apistg.rotalo.co:1443/gateway',
     'inactivityLimit': 86400 // seconds
-  };
+  };*/
 
   /*
   api = {
@@ -27,7 +27,7 @@ export class ConfigurationService {
     inactivityLimit: 86400 // seconds
   };*/
 
- /* api = {
+  api = {
     protocol: "https",
     server: "apirtl.rotalo.co/gateway/v1",
     inactivityLimit: 86400 // seconds
@@ -37,7 +37,7 @@ export class ConfigurationService {
     protocol: "https",
     server: "apirtl.rotalo.co/gateway",
     inactivityLimit: 86400 // seconds
-  };*/
+  };
 
   jsonApiSapiHeaders = {
     "Access-Control-Allow-Origin": "*",
