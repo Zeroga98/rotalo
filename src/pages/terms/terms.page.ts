de36f1d2bb44c71c6a4feb57b0dbbb058b7d4e6c
Mario Fernandez <jorge.fernandez@pragma.com.co>
2018-06-07 14:03:40 -0500

    se agrega footer en las paginas iniciales(sin autenticacion)

--- src/pages/terms/terms.page.ts
+++ src/pages/terms/terms.page.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { CurrentSessionService } from '../../services/current-session.service';
 @Component({
     selector: 'terms-page',
     templateUrl: './terms.page.html',
     styleUrls: ['./terms.page.scss']
 })
 export class TermsPage{
-    
+  public showHeader = false;
+  constructor(private currentSessionService: CurrentSessionService) {
+    if (this.currentSessionService.currentUser()) {
+      this.showHeader = true;
+    }
+  }
 }
