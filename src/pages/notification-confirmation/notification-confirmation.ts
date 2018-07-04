bba09715397f91334491199062595cd79fb7a531
Sebastian Zapata Colorado <sebastian.zapata@sebastianzapata1.local>
2018-07-04 11:44:48 -0500

    changing styles and request service

--- src/pages/notification-confirmation/notification-confirmation.ts
+++ src/pages/notification-confirmation/notification-confirmation.ts
@@ -13,19 +13,19 @@ import { MessagesService } from "../../services/messages.service";
 
 export class NotificationConfirmation implements OnInit {
     
-    private userId: number = parseInt(this.router.url.replace(/[^\d]/g, ''));
+    private userIdEncrypted: string = String(decodeURIComponent(this.router.url.split("?id=", 2)[1]));
 
-    constructor(private router: Router, private messagesService: MessagesService
-    ){
-    }
+    constructor(private router: Router, 
+        private messagesService: MessagesService
+    ){}
 
     ngOnInit() {
-        this.sendUserId();
+        this.sendUserIdEncrypted();
     }
 
-    sendUserId(){
+    sendUserIdEncrypted(){
         const jsonContent = {
-            "idUsuario": this.userId,
+            "idUsuario": this.userIdEncrypted,
             "tipoCorreo": "notificaciones_pendientes",
             "pararEnvioCorreo": true
         }
