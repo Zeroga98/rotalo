1e67f23a63469f3ec963d6673584609567dac468
Mario Fernandez <jorge.fernandez@pragma.com.co>
2018-08-08 11:55:54 -0500

    se agrega componente para mostrar modal de compartir producto

---
+++ src/components/modal-shareProduct/modal-shareProduct.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'modal-shareProduct',
+  templateUrl: './modal-shareProduct.component.html',
+  styleUrls: ['./modal-shareProduct.component.scss']
+})
+export class ModalShareProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
