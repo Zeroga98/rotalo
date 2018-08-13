9878cbc1d9558f2854ec4891c09dce35f26c2d9b
Mario Fernandez <jorge.fernandez@pragma.com.co>
2018-08-13 10:55:09 -0500

    se agregan estilos modal compartir

--- src/components/modal-shareProduct/modal-shareProduct.component.ts
+++ src/components/modal-shareProduct/modal-shareProduct.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy, Input, ElementRef } from '@angular/core';
 import { ModalShareProductService } from './modal-shareProduct.service';
+import { Validators, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'modal-shareProduct',
@@ -8,8 +9,12 @@ import { ModalShareProductService } from './modal-shareProduct.service';
 })
 export class ModalShareProductComponent implements OnInit, OnDestroy {
   @Input() id: string;
-
+  public sendInfoProduct;
+  public messageSuccess: boolean;
+  public messageError: boolean;
+  public textError: boolean;
   private element: any;
+
   constructor(private modalService: ModalShareProductService, private el: ElementRef) {
     this.element = el.nativeElement;
   }
@@ -23,6 +28,21 @@ export class ModalShareProductComponent implements OnInit, OnDestroy {
     }
     document.body.appendChild(this.element);
     this.modalService.add(this);
+    this.initShareForm();
+  }
+
+  initShareForm() {
+    this.sendInfoProduct = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)])
+    });
+  }
+
+  shareProduct() {
+    if (!this.sendInfoProduct.invalid) {
+      const params = {
+        correo: this.sendInfoProduct.get('email').value
+      };
+    }
   }
 
   ngOnDestroy(): void {
@@ -32,14 +52,11 @@ export class ModalShareProductComponent implements OnInit, OnDestroy {
 
 
   open(): void {
-    // this.element.style.display = 'block';
-
      this.element.classList.add('md-show');
      document.body.classList.add('modal-open');
    }
 
    close(): void {
-    //this.element.style.display = 'none';
      this.element.classList.remove('md-show');
      document.body.classList.remove('modal-open');
    }
