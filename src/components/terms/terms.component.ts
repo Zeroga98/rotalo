0a08df1fc33ca32ee810ab59b511bbb1a2d55f9b
Mario Fernandez <jorge.fernandez@pragma.com.co>
2018-09-03 14:46:34 -0500

    se agrega validacion para mostrar termino y condiciones para guatemala

--- src/components/terms/terms.component.ts
+++ src/components/terms/terms.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'terms',
   templateUrl: './terms.component.html',
   styleUrls: ['./terms.component.scss']
 })
 export class TermsComponent implements OnInit {
-
-  constructor() { }
+  private currentUrl = '';
+  constructor() {
+    this.currentUrl = window.location.href;
+  }
 
   ngOnInit() {
+    window.scrollTo(0, 0);
+  }
+
+  get isGuatemala() {
+    if (this.currentUrl.includes('gt')) {
+      return true;
+    }
+    return false;
   }
 
 }
