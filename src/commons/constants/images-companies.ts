d01bcca1794d672c7d16b724f861246011f53c80
Mario Fernandez <jorge.fernandez@pragma.com.co>
2018-09-28 17:54:23 -0500

    se agregan imagenes de las empresas

---
+++ src/commons/constants/images-companies.ts
@@ -0,0 +1,8 @@
+export const IMGS_COMPANIES = [
+  './assets/img/logos_company/argos@2x.png',
+  './assets/img/logos_company/bancolombia@2x.png',
+  './assets/img/logos_company/chef@2x.png',
+  './assets/img/logos_company/farbicato@2x.png',
+  './assets/img/logos_company/fec@2x.png',
+  './assets/img/logos_company/haceb@2x@2x.png',
+];
--- src/components/company-slider/company-slider.component.ts
+++ src/components/company-slider/company-slider.component.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { CAROUSEL_COMPANY_CONFIG } from './carouselCompany.config';
-import { NgxCarousel } from 'ngx-carousel';
+import { Component, OnInit } from "@angular/core";
+import { CAROUSEL_COMPANY_CONFIG } from "./carouselCompany.config";
+import { NgxCarousel } from "ngx-carousel";
+import { IMGS_COMPANIES } from "../../commons/constants/images-companies";
 
 @Component({
-  selector: 'company-slider',
-  templateUrl: './company-slider.component.html',
-  styleUrls: ['./company-slider.component.scss']
+  selector: "company-slider",
+  templateUrl: "./company-slider.component.html",
+  styleUrls: ["./company-slider.component.scss"]
 })
 export class CompanySliderComponent implements OnInit {
   public carouselCompanyConfig: NgxCarousel;
+  public imgCompaniesGroup = IMGS_COMPANIES;
+  public imgCompanies;
   constructor() {
     this.carouselCompanyConfig = CAROUSEL_COMPANY_CONFIG;
-   }
-
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.imgCompanies = this.chunkArray(this.imgCompaniesGroup, 4);
+  }
+
+  chunkArray(myArray, chunk_size) {
+    const results = [];
+    while (myArray.length) {
+      results.push(myArray.splice(0, chunk_size));
+    }
+    return results;
+  }
 }
