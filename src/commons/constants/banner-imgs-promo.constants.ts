482921a4990433a8e104a0e8d5cc8ef683158dcb
Mario Fernandez <jorge.fernandez@pragma.com.co>
2018-11-29 15:32:38 -0500

    se realiza despliegue en produccion

--- src/commons/constants/banner-imgs-promo.constants.ts
+++ src/commons/constants/banner-imgs-promo.constants.ts
@@ -1,16 +1,12 @@
 export const IMGS_BANNER_PROMO_BANCOLOMBIA = [
   './assets/img/banner/black_friday.jpg',
-//  './assets/img/banner/10k.gif',
  './assets/img/banner/banner_actualizaciones.jpg',
-  './assets/img/banner/fondos_de_empleados.jpg',
-  './assets/img/banner/banner_aliados.jpg',
+  './assets/img/banner/fondos_de_empleados.jpg'
 ];
 
 export const IMGS_BANNER_PROMO = [
   './assets/img/banner/black_friday.jpg',
-//  './assets/img/banner/10k.gif',
- './assets/img/banner/banner_actualizaciones.jpg',
-  './assets/img/banner/banner_aliados.jpg',
+ './assets/img/banner/banner_actualizaciones.jpg'
 ];
 
 export const IMGS_BANNER_GUATEMALA_PROMO = [
