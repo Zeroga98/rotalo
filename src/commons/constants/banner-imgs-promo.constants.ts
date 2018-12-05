fb4eeaef6e6548ccd04ed6c64ff6f10dee312fad
Mario Fernandez <jorge.fernandez@pragma.com.co>
2018-12-05 10:03:44 -0500

    se realiza cambio de banner reno

--- src/commons/constants/banner-imgs-promo.constants.ts
+++ src/commons/constants/banner-imgs-promo.constants.ts
@@ -1,12 +1,12 @@
 export const IMGS_BANNER_PROMO_BANCOLOMBIA = [
-  './assets/img/banner/banner_reno.jpg',
+  './assets/img/banner/banner_reno.gif',
 // './assets/img/banner/10k.gif',
  './assets/img/banner/banner_actualizaciones.jpg',
   './assets/img/banner/fondos_de_empleados.jpg'
 ];
 
 export const IMGS_BANNER_PROMO = [
-  './assets/img/banner/banner_reno.jpg',
+  './assets/img/banner/banner_reno.gif',
 //  './assets/img/banner/10k.gif',
  './assets/img/banner/banner_actualizaciones.jpg'
 ];
