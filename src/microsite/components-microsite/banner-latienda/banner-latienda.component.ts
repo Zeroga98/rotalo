f66a95b9336bc9806c2c31811667e97fa5bafba7
Mario Fernandez <jorge.fernandez@pragma.com.co>
2018-12-17 11:07:40 -0500

    cambio de banner micrositio bancolombia

--- src/microsite/components-microsite/banner-latienda/banner-latienda.component.ts
+++ src/microsite/components-microsite/banner-latienda/banner-latienda.component.ts
@@ -2,14 +2,13 @@ import {
     Component,
     OnInit
   } from '@angular/core';
-  
+
   @Component({
     selector: 'banner-latienda',
     templateUrl: 'banner-latienda.component.html',
     styleUrls: ['banner-latienda.component.scss']
   })
   export class BannerLatiendaComponent implements OnInit {
-  
     ngOnInit() {
     }
   }
