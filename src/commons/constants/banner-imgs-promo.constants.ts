047e87ecac42d57c6d9eaf845468270bb443f3cf
Mario Fernandez <jorge.fernandez@pragma.com.co>
2019-01-14 14:51:13 -0500

    se crean mejoras del sprint 18 en simular credito y el home

--- src/commons/constants/banner-imgs-promo.constants.ts
+++ src/commons/constants/banner-imgs-promo.constants.ts
@@ -1,12 +1,12 @@
 export const IMGS_BANNER_PROMO_BANCOLOMBIA = [
-  './assets/img/banner/banner_aliados.jpg',
   './assets/img/banner/hipotecario.jpg',
+  './assets/img/banner/banner_aliados.jpg',
   './assets/img/banner/banner_promo.jpg',
 ];
 
 export const IMGS_BANNER_PROMO = [
-  './assets/img/banner/banner_aliados.jpg',
   './assets/img/banner/hipotecario.jpg',
+  './assets/img/banner/banner_aliados.jpg',
   './assets/img/banner/banner_promo.jpg',
 ];
 
--- src/commons/constants/banner-imgs.contants.ts
+++ src/commons/constants/banner-imgs.contants.ts
@@ -1,12 +1,12 @@
 export const IMGS_BANNER = [
-  './assets/img/banner/banner_aliados.jpg',
   './assets/img/banner/hipotecario.jpg',
+  './assets/img/banner/banner_aliados.jpg',
   './assets/img/banner/banner_promo.jpg',
 ];
 
 export const IMGS_BANNER_MOBILE = [
-  './assets/img/banner/mobile/banner_aliados_mobile.jpg',
   './assets/img/banner/mobile/hipotecario_mobile.jpg',
+  './assets/img/banner/mobile/banner_aliados_mobile.jpg',
   './assets/img/banner/mobile/banner_promo_mobile.jpg',
 ];
 
@@ -15,7 +15,7 @@ export const IMGS_BANNER_GUATEMALA = [
 ];
 
 export const IMGS_BANNER_BANCOLOMBIA = [
-  './assets/img/banner/banner_aliados.jpg',
   './assets/img/banner/hipotecario.jpg',
+  './assets/img/banner/banner_aliados.jpg',
   './assets/img/banner/banner_promo.jpg',
 ];
