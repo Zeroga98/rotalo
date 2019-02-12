d7dbfc42a7d6b691c108267ce3cc6d851a9d648a
Mario Fernandez <jorge.fernandez@pragma.com.co>
2019-02-12 12:01:05 -0500

    se agregan estilos para formulario crear campaña

--- src/pages/profile/campaign-form/campaign-form.component.ts
+++ src/pages/profile/campaign-form/campaign-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DATAPICKER_CONFIG } from '../../../commons/constants/datapicker.config';
+import { IMyDpOptions } from 'mydatepicker';
 
 @Component({
   selector: 'campaign-form',
@@ -6,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./campaign-form.component.scss']
 })
 export class CampaignFormComponent implements OnInit {
-
+  datePickerOptions: IMyDpOptions = DATAPICKER_CONFIG;
   constructor() { }
 
   ngOnInit() {
