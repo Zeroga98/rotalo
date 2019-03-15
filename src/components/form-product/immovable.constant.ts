a3c9b9b71531570cda3ace17429e03b88df11ef8
Mario Fernandez <jorge.fernandez@pragma.com.co>
2019-03-15 15:41:04 -0500

    se agrega listado de antiguiedad para inmuebles

--- src/components/form-product/immovable.constant.ts
+++ src/components/form-product/immovable.constant.ts
@@ -12,5 +12,14 @@ export const PORTERIA = ['No Aplica', '12 Hrs', '24 Hrs'];
 
 export const PARQUEADERO = ['Si', 'No'];
 
-export const SOCIALCLASS = [1, 2, 3, 4, 5, 6]
+export const SOCIALCLASS = [1, 2, 3, 4, 5, 6];
+
+
+export const ANTIGUEDAD  = ['En contrucción',
+'A estrenar',
+'Hasta 5 años',
+'Entre 5 y 10 años',
+'Entre 10 y 20 años',
+'Entre 20 y 50 años',
+'Más de 50 años'];
 ;
