4efc11922e199a188a0eada44034ad7638be65d1
Mario Fernandez <jorge.fernandez@pragma.com.co>
2019-05-15 15:56:26 -0500

    se agregan opciones de tabla para productos tienda

--- src/pages/profile/products-shop/products-shop.component.ts
+++ src/pages/profile/products-shop/products-shop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { MatSort, MatTableDataSource } from '@angular/material';
 import * as moment from 'moment';
 import { UtilsService } from './../../../util/utils.service';
@@ -11,14 +11,15 @@ import { ProductsService } from '../../../services/products.service';
   templateUrl: './products-shop.component.html',
   styleUrls: ['./products-shop.component.scss']
 })
-export class ProductsShopComponent implements OnInit {
+export class ProductsShopComponent implements OnInit, AfterViewInit {
 
   dataSource;
   displayedColumns = ['photos', 'id', 'name', 'description', 'price', 'priceIVA', 'IVA', 'published-at', 'stock', 'status', 'edit'];
   @ViewChild(MatSort) sort: MatSort;
   public productsActive = [];
   public productsInactive = [];
-
+  public productos = [];
+  @ViewChild('grid') grid: ElementRef;
 
   constructor(private productsService: ProductsService,
     private utilService: UtilsService,
@@ -27,6 +28,10 @@ export class ProductsShopComponent implements OnInit {
 
   ngOnInit() {
     this.getProductsList();
+  }
+
+  ngAfterViewInit() {
+    let inputs = document.getElementsByTagName('tbody');
   }
 
   getFormatDate(date) {
