afe9e1afacd5983e4396ce7a13d49182e0aac22a
Jairo Dario <dairo.lopez@pragma.com.co>
2019-06-26 23:32:31 -0500

    integrar get banners en home tienda

--- src/microsite/components-microsite/banner-latienda/banner-latienda.component.ts
+++ src/microsite/components-microsite/banner-latienda/banner-latienda.component.ts
@@ -1,7 +1,11 @@
 import {
     Component,
-    OnInit
+    OnInit,
+    ViewChild,
+    ElementRef
   } from '@angular/core';
+  import { SettingsService } from '../../../services/settings.service';
+  import { FormBuilder, Validators, FormArray } from '@angular/forms';
 
   @Component({
     selector: 'banner-latienda',
@@ -9,6 +13,101 @@ import {
     styleUrls: ['banner-latienda.component.scss']
   })
   export class BannerLatiendaComponent implements OnInit {
+    
+    //@ViewChild("containerBanners", { read: ElementRef })
+    //containerBanners: ElementRef;
+    public bannerHomeTienda;
+    //public storeId = 1;
+    public bannersCategoriaForm;
+    public bannerPromocionalForm;
+    constructor(
+      private settingsService: SettingsService,
+      private formBuilder: FormBuilder
+    ) { }
     ngOnInit() {
+      this.loadBanners();
+      this.setFormHomeShop(this.getInitialConfigHomeShop());
     }
+
+    loadBanners(){
+      this.settingsService.getBannersShop(1).subscribe(response => {
+        if (response.body) {
+        if (response.body.bannerHomeTienda) {this.setFormHomeShop(response.body.bannerHomeTienda);}
+        if (response.body.bannerPromocional && response.body.bannerPromocional.length > 0) {this.setInitialFormPromo(response.body);}
+        if (response.body.bannersCategoria && response.body.bannersCategoria.length > 0) {this.setInitialFormCategories(response.body);}
+        }
+      });
+    }
+
+    private getInitialConfigHomeShop() {
+      const config =  {
+        'idLogo': '',
+        'urlLogo': '',
+        'idBannerDesktop': '',
+        'urlBannerDesktop': '',
+        'idBannerMobile': '',
+        'urlBannerMobile': ''
+      };
+      return config;
+    }
+
+    private setFormHomeShop(config) {
+      this.bannerHomeTienda = this.formBuilder.group({
+        'idLogo': [config.idLogo],
+        'urlLogo': [config.urlLogo],
+        'idBannerDesktop': [config.idBannerDesktop],
+        'urlBannerDesktop': [config.urlBannerDesktop],
+        'idBannerMobile': [config.idBannerMobile],
+        'urlBannerMobile': [config.urlBannerMobile]
+      });
+    }
+
+    private setInitialFormPromo(config) {
+      this.bannerPromocionalForm = this.formBuilder.group({
+        bannerPromocional: this.formBuilder.array(
+          this.createItemShop(config.bannerPromocional)
+        )
+      });
+    }
+  
+    private setInitialFormCategories(config) {
+      this.bannersCategoriaForm = this.formBuilder.group({
+       bannersCategoria: this.formBuilder.array(
+          this.createItem(config.bannersCategoria)
+        )
+      });
+    }
+
+    private createItemShop(bannersForm) {
+      const bannerPromocional = bannersForm.map(banner => {
+        return this.formBuilder.group({
+          idBannerPromocional: banner.idBannerPromocional,
+          idBannerDesktop: banner.idBannerDesktop,
+          urlBannerDesktop: banner.urlBannerDesktop,
+          idBannerMobile: banner.idBannerMobile,
+          urlBannerMobile: banner.urlBannerMobile,
+          idCategoria: banner.idCategoria,
+          link: banner.link
+        });
+      });
+      return bannerPromocional;
+    }
+  
+    private createItem(bannersForm) {
+      const bannersCategoria = bannersForm.map(banner => {
+        return this.formBuilder.group({
+          idBannerCategoria: banner.idBannerCategoria,
+          idBannerDesktop: banner.idBannerDesktop,
+          urlBannerDesktop: banner.urlBannerDesktop,
+          idBannerMobile: banner.idBannerMobile,
+          urlBannerMobile: banner.urlBannerMobile,
+          idCategoria: banner.idCategoria
+        });
+      });
+      return bannersCategoria;
+    }
+
+
   }
+
+  
