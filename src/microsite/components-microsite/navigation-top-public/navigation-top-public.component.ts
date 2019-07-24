fa3012f801630bc6316c8078dee5df83ff725299
Mario Fernandez <jorge.fernandez@pragma.com.co>
2019-07-24 09:20:03 -0500

    se agrega componente para header en tienda publica

---
+++ src/microsite/components-microsite/navigation-top-public/navigation-top-public.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'navigation-top-public',
+  templateUrl: './navigation-top-public.component.html',
+  styleUrls: ['./navigation-top-public.component.scss']
+})
+export class NavigationTopPublicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
