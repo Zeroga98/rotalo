01db4ef0e5db985362026335e45cb4fb4d759d5a
Mario <jorge.fernandez@pragma.com.co>
2019-08-07 22:08:17 -0500

    formulario de contacto tienda externa no es interactuable movil

--- src/microsite/components-microsite/detail-product-shop/detail-product-shop.component.ts
+++ src/microsite/components-microsite/detail-product-shop/detail-product-shop.component.ts
@@ -702,7 +702,7 @@ export class DetailProductShopComponent implements OnInit {
     dialogConfig.minWidth = '300px';
     dialogConfig.maxWidth = '900px';
     dialogConfig.minHeight = '494px';
-    dialogConfig.maxHeight = '90vh';
+   // dialogConfig.maxHeight = '90vh';
     dialogConfig.autoFocus = false;
    /* dialogConfig.autoFocus = true;
     dialogConfig.minWidth = '300px';
