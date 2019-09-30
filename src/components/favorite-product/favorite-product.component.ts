ecce3b681080e401049da2858efeafec4af289ce
Mario Fernandez <jorge.fernandez@pragma.com.co>
2019-09-30 15:08:08 -0500

    funciones para redireccionar al producto favorito

--- src/components/favorite-product/favorite-product.component.ts
+++ src/components/favorite-product/favorite-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { ROUTES } from '../../router/routes';
 
 @Component({
   selector: 'favorite-product',
@@ -7,7 +9,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FavoriteProductComponent implements OnInit {
   @Input() product;
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
@@ -16,9 +18,22 @@ export class FavoriteProductComponent implements OnInit {
     return ('url(' + photo.replace(/ /g, '%20') + ')') ;
   }
 
-
   get isProductAvailable() {
-    return this.product.status == 'Disponible';
+    return this.product && this.product.status == 'Disponible';
+  }
+
+  get isCostume () {
+    if ((this.product['product_subcategory_id'] &&
+    (this.product['product_subcategory_id'] == 77 || this.product['product_subcategory_id'] == 127))) {
+      return true;
+    }
+    return false;
+  }
+
+  goToProduct() {
+    this.router.navigate([
+      `/${ROUTES.PRODUCTS.LINK}/${ROUTES.PRODUCTS.SHOW}/${this.product.productId}`
+    ]);
   }
 
 
